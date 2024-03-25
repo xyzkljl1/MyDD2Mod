cfee63281eb5772da67a6c0d2ea1ae65ae830a65
xyzkljl1 <xyzkljl1@yahoo.com>
2024-03-26 03:06:29 +0800

    fasterrun add speed in battle

--- FasterRun/reframework/autorun/FasterRun.lua
+++ FasterRun/reframework/autorun/FasterRun.lua
@@ -1,4 +1,4 @@
-local modname="[BattleInfo]"
+local modname="[FasterRun]"
 log.info(modname.."Start")
 local myLog="LogStart\n"
 local config = json.load_file("FasterRun.json") or {}
@@ -6,9 +6,10 @@ if config.Speed1==nil then config.Speed1=9 end
 if config.Speed2==nil then config.Speed2=30 end
 if config.Speed3==nil then config.Speed3=30 end
 if config.Speed4==nil then config.Speed4=46 end
+if config.Speed5==nil then config.Speed5=30 end
 
 local function Log(msg)
-    --myLog = myLog .."\n".. msg
+    myLog = myLog .."\n".. msg
     log.info(modname..msg)
 end
 local function ClearLog()
@@ -38,7 +39,6 @@ end
 --end)
 
 
-
 sdk.hook(
     sdk.find_type_definition("app.GuiManager"):get_method("OnChangeSceneType"),
     function() end,
@@ -49,11 +49,13 @@ sdk.hook(
             Log("Init Move Speed2")
             local speedpara=player:get_Human():get_Param():get_Speed()
             local list=speedpara.SpeedDataList
-            --6.047/17.71/23.32/26.92
+            --6.047/17.71/23.32/26.92/-/15.98
             list[0].BaseSpeed=config.Speed1--徐行(轻推摇杆)
             list[1].BaseSpeed=config.Speed2--走路（推摇杆）
             list[2].BaseSpeed=config.Speed3--持刀冲刺
             list[3].BaseSpeed=config.Speed4--收刀冲刺
+            --4不知道是什么
+            list[5].BaseSpeed=config.Speed5--战斗中走路
         end
     end
 )
