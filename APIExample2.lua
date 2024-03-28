46a5da21fc295383edd64b49c9387303b66e891f
xyzkljl1 <xyzkljl1@yahoo.com>
2024-03-29 04:31:45 +0800

    update example

--- APIExample2.lua
+++ APIExample2.lua
@@ -1,7 +1,6 @@
 -------
 local modname="ExampleModUsingHotkeyAndInit"
 local configfile=modname..".json"
-log.info("["..modname.."]".."Start")
 --settings
 local _config={
     {name="para1",type="font",default="simsun.ttc"},
@@ -18,13 +17,13 @@ local config=myapi.InitFromFile(_config,configfile)
 
 ------Mod Implement
 re.on_frame(function()
-    draw.text("para1="..tostring(config.para1),50,50,config.para3)
-    draw.text("para2="..tostring(config.para2),50,80,config.para3)
+    draw.text("para1="..tostring(config.para1),50,50, 0xffffffff)
+    draw.text("para2="..tostring(config.para2),50,80,0xffffffff)
     if hk.check_hotkey("keyboardKey7784",false,true) or ((hk.check_hotkey("controllerKeyShoulder7784",  true,false) and hk.check_hotkey("controllerKeyNotShoulder7784",  false,true))) then
-        draw.text("key Triggerer")
+        draw.text("key Triggerer",50,100, 0xffffffff)
     end
     if hk.check_hotkey("keyboardKey7784",true) then
-        draw.text("KeyDown")
+        draw.text("KeyDown",50,120, 0xffffffff)
     end
 
 end)
