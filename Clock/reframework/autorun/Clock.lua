390384996d998c968579e5acc785612d5705f2c2
xyzkljl1 <xyzkljl1@yahoo.com>
2024-03-28 21:34:26 +0800

    add ui api clock: use ui api

--- Clock/reframework/autorun/Clock.lua
+++ Clock/reframework/autorun/Clock.lua
@@ -1,21 +1,37 @@
-local modname="[Clock]"
-log.info(modname.."Start")
-local myLog="LogStart\n"
-
-local config = json.load_file("Clock.json") or {}
-if config.fontsize==nil then config.fontsize=60 end
-if config.color==nil then config.color=0xffEEEEEE end
-if config.offsetX==nil then config.offsetX=50 end
-if config.offsetY==nil then config.offsetY=50 end
+local modname="Clock"
+local configfile=modname..".json"
+log.info("["..modname.."]".."Start")
+--settings
+local _config={
+    {name="fontsize",type="int",default=60,min=1,max=250,needrestart=true},
+    {name="offset",type="intN",default={50,50},min=-300,max=8000},
+    {name="color",type="rgba32",default=0xffEEEEEE},
+}
+--merge config file to default config
+local function recurse_def_settings(tbl, new_tbl)
+	for key, value in pairs(new_tbl) do
+		if type(tbl[key]) == type(value) then
+		    if type(value) == "table" then
+			    tbl[key] = recurse_def_settings(tbl[key], value)
+            else
+    		    tbl[key] = value
+            end
+		end
+	end
+	return tbl
+end
+local config = {} 
+for key,para in pairs(_config) do
+    config[para.name]=para.default
+end
+config= recurse_def_settings(config, json.load_file(configfile) or {})
+--On setting Change
+local function OnChanged()
+end
 
 local font = imgui.load_font("times.ttf", config.fontsize)
 local function Log(msg)
-    myLog = myLog .."\n".. msg
     log.info(modname..msg)
-end
-local function ClearLog()
-    --draw.text(myLog,50,50,0xffEEEEFE)
-    myLog = ""
 end
 
 re.on_frame(function()
@@ -39,8 +55,18 @@ re.on_frame(function()
         end
         --2sec for 1min?
         local msg=string.format("%dDay %s %d:%d:%d",d,state,h,m,math.floor(s)%2)
-        draw.text(msg,config.offsetX,config.offsetY,config.color) 
+        draw.text(msg,config.offset[1],config.offset[2],config.color) 
     end
     imgui.pop_font()
-    ClearLog()
 end)
+
+
+
+--try load api and draw ui
+local function prequire(...)
+    local status, lib = pcall(require, ...)
+    if(status) then return lib end
+    return nil
+end
+local myapi = prequire("_XYZApi/_XYZApi")
+if myapi~=nil then myapi.DrawIt(modname,configfile,_config,config,OnChanged) end
---
+++ _XYZApi/_XYZApi.lua
@@ -0,0 +1,78 @@
+local vecNames={
+    ".x",
+    ".y",
+    ".z",
+    ".w"
+}
+
+local function DrawIt(modname,configfile,_config,config,OnChange)
+    re.on_draw_ui(function()
+        local changed=false
+        local _changed=false
+	    if imgui.tree_node(modname) then	
+		    --imgui.same_line()
+		    --imgui.text("*Right click on most options to reset them")		
+		    imgui.begin_rect()
+		
+            for idx,para in ipairs (_config) do
+                local key=para.name
+                local title_postfix=""
+                if para.needrestart==true then
+                    title_postfix=" (Need Restart To Apply)"
+                end
+
+                if para.type=="int" then
+        		    changed , config[key]= imgui.drag_int(key .. title_postfix, 
+                                                            config[key] or para.default or para.min or 0,
+                                                            para.step or 1 , para.min or 0, para.max)
+                    _changed=changed or _changed
+                elseif para.type=="intN" then
+                    imgui.push_item_width(215)
+                    for _k,_ in pairs (config[key]) do
+                        local vname=vecNames[_k] or ".".._k
+            		    changed , config[key][_k] = imgui.drag_int(key..vname .. title_postfix, 
+                                                             config[key][_k] or para.default or para.min or 0,
+                                                             para.step or 1 , para.min or 0, para.max)
+                        _changed=changed or _changed
+                        imgui.same_line()
+                    end
+                    imgui.pop_item_width()
+                    imgui.new_line()
+                elseif para.type=="rgba32" then
+                    local value=config[key] or para.default or para.min or 0xffffffff
+                    local a=(value & 0xff000000) >>24
+                    local b=(value & 0x00ff0000) >>16
+                    local g=(value & 0x0000ff00) >>8
+                    local r=value & 0xff
+                    imgui.push_item_width(80)
+                    changed , r= imgui.drag_int(key..".R" .. title_postfix, r,1 , 0, 255)
+                    _changed=changed or _changed
+                    imgui.same_line()
+                    changed , g= imgui.drag_int(key ..".G".. title_postfix, g,1 , 0, 255)
+                    _changed=changed or _changed
+                    imgui.same_line()
+                    changed , b= imgui.drag_int(key ..".B".. title_postfix, b,1 , 0, 255)
+                    _changed=changed or _changed
+                    imgui.same_line()
+                    changed , a= imgui.drag_int(key ..".A".. title_postfix, a,1 , 0, 255)
+                    _changed=changed or _changed
+                    imgui.pop_item_width()
+                    config[key]=(a<<24)|(r)|(g<<8)|(b<<16)
+                end
+            end
+		    imgui.tree_pop()
+        end
+        if _changed==true then
+            json.dump_file(configfile, config)
+            if OnChange~=nil then
+                OnChange()
+            end
+        end
+    end)
+end
+
+
+_XYZApi={
+    DrawIt=DrawIt
+}
+return _XYZApi
