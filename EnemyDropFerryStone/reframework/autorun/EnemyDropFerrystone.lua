9094aebfd452967325a484c583304b1570969476
xyzkljl1 <xyzkljl1@yahoo.com>
2024-03-31 02:15:43 +0800

    EnemyDropFerryStone: ignore beetles/add option to disable gimmick modify

--- EnemyDropFerryStone/reframework/autorun/EnemyDropFerrystone.lua
+++ EnemyDropFerryStone/reframework/autorun/EnemyDropFerrystone.lua
@@ -6,6 +6,8 @@ local eachRate={0,0,0,0,0}
 local DropListCT=5
 
 local _config={
+    {name="AffectGatherSpot",type="bool",default=true},
+
     {name="item1",type="item",default=80},
     {name="count1",type="int",default=1,min=0,max=99},
     {name="rate1",type="int",default=5,min=0,max=100},
@@ -41,29 +43,85 @@ myItem._Id=1
 myItem._Num=1
 myItem._Rate=1
 
+local isLootingGimmick=false
+local isLootingBody=false
+
+
+sdk.hook(
+    sdk.find_type_definition("app.ItemDropParam"):get_method("getFumbleLotItem"),
+    function(args)
+        local this=sdk.to_managed_object(args[2])
+        local gid=this:get_GimmickId()
+        --print(gid)
+        if gid ~=0 then
+            --never hit
+        else
+            isLootingBody=true
+            Log("Start Looting Body")
+        end
+    end,
+    function (retval)
+        Log("End Looting Body")
+        isLootingBody=false
+        return retval
+    end
+)
+sdk.hook(
+    sdk.find_type_definition("app.Gm82_009"):get_method("giveItem"),
+    function(args)
+        local this=sdk.to_managed_object(args[2])
+        local gid=this.GimmickId
+        --161 ¼×³æ
+        if gid ~=0 and gid~=161 then
+            isLootingGimmick=true
+            Log("Start Looting Gimmick "..gid)
+        end
+    end,
+    function (retval)
+        Log("End Looting Gimmick")
+        isLootingGimmick=false
+        return retval
+    end
+)
+
+
 --getLotItem not seems to have non-nil retval?
+--app.SearchDeadBodyInteractController.executeInteract(System.UInt32, app.Character)
+--app.ItemDropParam.getFumbleLotItem(app.GatherContext, System.Int32, System.Int32)
+-- beetle/Â·±ßµÄ²Ý is called by giveItem,no fumbleLotItem
+--app.Gm82_009.giveItem(app.Character, System.Boolean)
 sdk.hook(
     sdk.find_type_definition("app.ItemDropParam.Table"):get_method("getLotItemSub"),
     nil,
     function (retval)
         local item=sdk.to_managed_object(retval)
-        if item:get_IsOnce()==true or item:get_IsSpecialOnce()==true then 
-            Log("Ignore Once Item")
-            return retval 
-        end
-        --roll a dice
-        local random= math.random(0,99)
-        for i=1,DropListCT do
-            if eachRate[i] > random then
-                local istr=tostring(i)
-                myItem._Id = config['item'..istr]
-                myItem._Num = config['count'..istr]
-                Log("Rate: ",random," Replace Drop With "..i)
-                return sdk.to_ptr(myItem)
+        if isLootingBody or (isLootingGimmick and config.AffectGatherSpot) then
+            if item:get_IsOnce()==true or item:get_IsSpecialOnce()==true then 
+                Log("Ignore Once Item")
+                return retval 
             end
+            if item._Id==398 then
+                --shouldn't hit
+                Log("Ignore Beetle")
+                return retval
+            end
+
+            --roll a dice
+            local random= math.random(0,99)
+            for i=1,DropListCT do
+                if eachRate[i] > random then
+                    local istr=tostring(i)
+                    myItem._Id = config['item'..istr]
+                    myItem._Num = config['count'..istr]
+                    Log("Rate: ",random," Replace Drop With "..i)
+                    return sdk.to_ptr(myItem)
+                end
+            end
+            Log("Rate: ",random," No Replace ",item._Id)
+            --original
+            return retval
         end
-        Log("Rate: ",random," No Replace ",item._Id)
-        --original
+        Log("Ignore")
         return retval
     end
 )
